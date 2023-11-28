099d5429f41b9fabb7bf96b347d5cdb50ca8bb3f
Antti2023 <125262005+Antti2023@users.noreply.github.com>
2023-11-28 09:58:44 +0200

    Update lampotilamuunnin.js

--- lampotilamuunnin.js
+++ lampotilamuunnin.js
@@ -1,47 +1,47 @@
 function muunnaftoc(F) {
     if (F < -459.67) {
         alert("Huomio: Lämpötila on pienempi kuin absoluuttinen nollapiste.");
-        // Palautetaan 0, jos ehto ei täyty
+        
         return 0;
     }
-    // Fahrenheit	Celsius	°C = (°F − 32) / 1,8
+    
     return (F - 32) / 1.8
 }
 
 function muunnactof(C) {
     if (C < -273.15) {
         alert("Huomio: Lämpötila on pienempi kuin absoluuttinen nollapiste.");
-        // Palautetaan 0, jos ehto ei täyty
+        
         return 0;
     }
-    //  Celsius	Fahrenheit	°F = (°C) · 1,8 + 32
+    
     return (C * 1.8) + 32
 }
 
 function muunnaftok(F) {
     if (F < -459.67) {
         alert("Huomio: Lämpötila on pienempi kuin absoluuttinen nollapiste.");
-        // Palautetaan 0, jos ehto ei täyty
+        
         return 0;
     }
-    // Fahrenheit	Kelvin	K = (°F + 459,67) / 1,8
+   
     return (F + 459.67) / 1.8;
 }
 function muunnaktof(K) {
     if (K < 0) {
         alert("Huomio: Lämpötila on pienempi kuin absoluuttinen nollapiste.");
-        // Palautetaan 0, jos ehto ei täyty
+        
         return 0;
     }
-    // Kelvin	Fahrenheit	°F = K · 1,8 − 459,67
+    
     return (K * 1.8) - 459.67
 }
 
 function muunnaktoc(K) {
-    //Kelvin	Celsius	°C = K − 273,15
+   
     if (K < 0) {
         alert("Huomio: Lämpötila on pienempi kuin absoluuttinen nollapiste.");
-        // Palautetaan 0, jos ehto ei täyty
+       
         return 0;
     }
     return K - 273.15
@@ -50,19 +50,19 @@ function muunnaktoc(K) {
 function muunnactok(C) {
     if (C < -273.15) {
         alert("Huomio: Lämpötila on pienempi kuin absoluuttinen nollapiste.");
-        // Palautetaan 0, jos ehto ei täyty
+        
         return 0;
     }
-    // Celsius	Kelvin	K = °C + 273,15
+    
     return C + 273.15
 }
 
 function muunnaLampotila() {
 
-    // hae käyttäjän syöttämä lämpötila
+    
     const lampotila = parseFloat(document.getElementById("lampotila-input").value);
     console.log("Lämpölila-input", lampotila);
-    // hae käyttäjän valitsema muunnos
+   
     if (isNaN(lampotila)) {
         alert("Virhe: Anna kelvollinen lämpötila.");
         return;
@@ -97,5 +97,5 @@ function muunnaLampotila() {
     console.log("Tulos:", lampotilamuunnos);
     const resultElement = document.getElementById("result");
     resultElement.innerHTML = "lämpötila: " + lampotilamuunnos.toFixed(document.getElementById("lämpötila")["desimaali"].value);
-    // kutsu oikeaa muunnosfunktiota
+    
 }
